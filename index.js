9442ae46bb86c2c321911c0c9fc299848b8f2b13
CyberL1 <mateusz.hf18@gmail.com>
2023-11-02 08:59:34 +0100

    Add links.json and set activity

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import "dotenv/config";
 import { Client } from "discord.js";
+import { readFileSync } from "node:fs";
 
 const client = new Client({ intents: ["Guilds", "GuildMessages", "MessageContent"] });
 
+const links = JSON.parse(readFileSync("links.json"));
+
 client.once("ready", () => {
+  client.user.setActivity(`${Object.keys(links).length} links`, { type: 3 });
   console.log(`${client.user.username} ready`);
 });
 
