75b481098beef01e71f9cfc239f8301d54e7f016
CyberL1 <mateusz.hf18@gmail.com>
2023-11-03 23:11:22 +0100

    Add info command

---
+++ commands/info/index.js
@@ -0,0 +1,11 @@
+import { EmbedBuilder } from "discord.js";
+
+export default interaction => {
+  const embed = new EmbedBuilder()
+    .setColor("DarkBlue")
+    .setAuthor({ name: "Link replacer", iconURL: interaction.client.user.avatarURL() })
+    .setDescription("Link replacer is a bot made for DDevs Buildathon 2023\nIt replaces links as the name implies")
+    .setFooter({ text: `2023-${new Date().getUTCFullYear()}` });
+
+  interaction.reply({ embeds: [embed] });
+};
