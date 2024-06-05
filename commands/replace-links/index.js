0accfc66fd36cbf8565c90724986dfe48026b739
CyberL1 <mateusz.hf18@gmail.com>
2024-06-05 15:59:08 +0200

    fix: check if message has no links to replace or no content

--- commands/replace-links/index.js
+++ commands/replace-links/index.js
@@ -4,10 +4,24 @@ export default async (interaction) => {
   const channel = await interaction.client.channels.fetch(
     interaction.channelId,
   );
+
   const message = await channel.messages.fetch(interaction.targetId);
 
+  if (!message.content) {
+    return interaction.reply({
+      content: "This message has no content",
+      ephemeral: true,
+    });
+  }
+
   const replaced = replaceLinks(message, interaction.client.links);
-  console.log(replaced);
+
+  if (!replaced) {
+    return interaction.reply({
+      content: "No links to replace",
+      ephemeral: true,
+    });
+  }
 
   const links = replaced
     .map(
