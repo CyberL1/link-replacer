0767d6341e567f3ba2e0cda79792135a9c6125d9
CyberL1 <mateusz.hf18@gmail.com>
2024-06-05 19:14:00 +0200

    fix: get message from interaction

--- commands/replace-links/index.js
+++ commands/replace-links/index.js
@@ -1,34 +1,33 @@
 import { replaceLinks } from "../../utils/messages.js";
 
 export default async (interaction) => {
-  const channel = await interaction.client.channels.fetch(
-    interaction.channelId,
-  );
+  if (interaction.isMessageContextMenuCommand()) {
+    if (!interaction.targetMessage.content) {
+      return interaction.reply({
+        content: "This message has no content",
+        ephemeral: true,
+      });
+    }
 
-  const message = await channel.messages.fetch(interaction.targetId);
+    const replaced = replaceLinks(
+      interaction.targetMessage,
+      interaction.client.links,
+    );
 
-  if (!message.content) {
-    return interaction.reply({
-      content: "This message has no content",
-      ephemeral: true,
-    });
+    if (!replaced) {
+      return interaction.reply({
+        content: "No links to replace",
+        ephemeral: true,
+      });
+    }
+
+    const links = replaced
+      .map(
+        ({ protocol, hostname, pathname }) =>
+          `<${protocol}//${hostname}${pathname}> -> ${protocol}//${interaction.client.links[hostname]}${pathname}`,
+      )
+      .join("\n");
+
+    interaction.reply({ content: links });
   }
-
-  const replaced = replaceLinks(message, interaction.client.links);
-
-  if (!replaced) {
-    return interaction.reply({
-      content: "No links to replace",
-      ephemeral: true,
-    });
-  }
-
-  const links = replaced
-    .map(
-      ({ protocol, hostname, pathname }) =>
-        `<${protocol}//${hostname}${pathname}> -> ${protocol}//${interaction.client.links[hostname]}${pathname}`,
-    )
-    .join("\n");
-
-  interaction.reply({ content: links });
 };
